fe3cfb8f9c40b713801b96834f5ccf96aa4209b7
dial1201 <fancy_95310hotmail.com>
2021-04-16 20:33:50 +0200

    create a new controller and create psr-4 and implementation callable

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controller\HelloController;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
@@ -9,12 +10,14 @@ use Symfony\Component\Routing\RouteCollection;
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+
 $listeRoute = new Route('/');
-$createRoute = new Route('/create');
+$createRoute = new Route('/create', [], [], [], 'localhost', ['http'], ['POST', 'GET']);
 $showRoute = new Route('/show/{id<\d+>?100}');
 $helloRoute = new Route(
     '/hello/{name}',
-    ['name' => 'World', 'parameters' => '17']
+    ['name' => 'World', 'controller' => [new HelloController, 'sayHello']]
 );
 
 $collection = new RouteCollection;
@@ -35,6 +38,11 @@ try {
     $currentRoute = $matcher->match($pathInfo);
 
     dump($currentRoute);
+
+    $controller = $currentRoute['controller'];
+    call_user_func($controller, $currentRoute);
+    die();
+
     $page = $currentRoute['_route'];
 
     require_once "pages/$page.php";
@@ -42,49 +50,3 @@ try {
     require 'pages/404.php';
     return;
 }
-
-
-
-/**
- * LES PAGES DISPONIBLES
- * ---------
- * Afin de pouvoir être sur que le visiteur souhaite voir une page existante, on maintient ici une liste des pages existantes
- */
-// $availablePages =  [
-//     'list', 'show', 'create'
-// ];
-
-// Par défaut, la page qu'on voudra voir si on ne précise pas (par exemple sur /index.php) sera "list"
-// $page = 'list';
-
-// Si on nous envoi une page en GET, on la prend en compte (exemple : /index.php?page=create)
-// if (isset($_GET['page'])) {
-//     $page = $_GET['page'];
-// }
-
-// Si la page demandée n'existe pas (n'est pas dans le tableau $availablePages)
-// On affiche la page 404
-// if (!in_array($page, $availablePages)) {
-//     require 'pages/404.php';
-//     return;
-// }
-
-/**
- * ❌ ATTENTION DEMANDEE !
- * -----------
- * Ici, un moyen simple d'obeir au visiteur et de lui présenter ce qu'il demande c'est d'inclure le fichier qui porte le même nom que la 
- * variable $page. 
- * 
- * => EXTREMENT DANGEREUX ! Ca veut dire que le visiteur pilote l'inclusion de scripts PHP, quelqu'un de malin pourrait s'en servir pour inclure 
- * un script non prévu ou voulu. On est un peu protégé par la condition juste au dessus, mais c'est quand même HYPER LIMITE.
- * 
- * Comment allons nous réparer ça dans les prochaines sections ?
- * 
- * ❌ AUTRE PROBLEME DE TAILLE ICI : LE COUPLAGE DE L'URL ET DES NOMS DE FICHIERS
- * ------------
- * Le fichier que l'on va inclure porte le même nom que le paramètre $_GET['page']. C'est à dire que si on appelle /index.php?page=create
- * c'est le fichier pages/create.php qui va être inclus.
- * 
- * La conséquence, c'est que si demain je décide que le formulaire de création devrait se trouver sur /index.php?page=new il faudra que je
- * renomme forcément le fichier pages/create.php en pages/new.php et inversement (l'enfer)
- */
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -21,6 +21,10 @@ class ComposerStaticInit049e76bc028b703bf5513b0e3acbefba
             'Symfony\\Component\\VarDumper\\' => 28,
             'Symfony\\Component\\Routing\\' => 26,
         ),
+        'A' => 
+        array (
+            'App\\' => 4,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -39,6 +43,10 @@ class ComposerStaticInit049e76bc028b703bf5513b0e3acbefba
         'Symfony\\Component\\Routing\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/routing',
+        ),
+        'App\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/src',
         ),
     );
 
