ed691ebcd5bd5953d21127fe7f62abf4ee33e46f
dial1201 <fancy_95310hotmail.com>
2021-04-17 15:46:45 +0200

    refactoring

--- index.php
+++ index.php
@@ -1,47 +1,8 @@
 <?php
 
-use App\Controller\TaskController;
-use App\Controller\HelloController;
-use App\Loader\CustomAnnotationClassLoader;
-use Doctrine\Common\Annotations\AnnotationReader;
-use Doctrine\Common\Annotations\Reader;
-use Doctrine\Common\Annotations\AnnotationRegistry;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Loader\PhpFileLoader;
-use Symfony\Component\Routing\Loader\YamlFileLoader;
-use Symfony\Component\Routing\Generator\UrlGenerator;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
-use Symfony\Component\Routing\Loader\AnnotationFileLoader;
 
-require __DIR__ . '/vendor/autoload.php';
-
-// $loader = new PhpFileLoader(new FileLocator(__DIR__ . '/config'));
-// $collection = $loader->load('routes.php');
-
-// $loader = new YamlFileLoader(new FileLocator(__DIR__ . '/config'));
-// $collection = $loader->load('routes.yaml');
-
-$classLoader = require __DIR__ . '/vendor/autoload.php';
-AnnotationRegistry::registerLoader('class_exists');
-
-$loader = new AnnotationDirectoryLoader(
-    new FileLocator(__DIR__ . '/src/Controller'),
-    new CustomAnnotationClassLoader(new AnnotationReader())
-);
-
-$collection = $loader->load(__DIR__ . '/src/Controller');
-
-$matcher = new UrlMatcher($collection, new RequestContext());
-
-$generator = new UrlGenerator($collection, new RequestContext());
-
-
-$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+require __DIR__ . '/config/start.php';
 
 try {
     $currentRoute = $matcher->match($pathInfo);
